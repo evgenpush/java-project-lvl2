c31705c52f7c78f8ef8320e23dfc6bfa6ac218c7
Evgeny Pushkin <seppuku@yandex.ru>
2022-03-25 23:13:28 +0700

    add Differ

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,9 +1,39 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import java.util.concurrent.Callable;
+import java.util.*;
+
+@Command(name = "gendiff ",  version = "App 1.0", mixinStandardHelpOptions = true,
+         description = "Compares two configuration files and shows a difference.")
+
+class App implements Callable<Integer> {
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "my format";
+
+    @Parameters(paramLabel = "filepatch1", description = "patch to first file")
+    private String file1;
+
+    @Parameters(paramLabel = "filepatch2", description = "patch to second file")
+    private String file2;
+
+    @Override
+    public Integer call() throws Exception {
+
+        String test = Differ.generate(file1, file2);
+
+        System.out.println(test);
+        return 0;
+    }
 
 
 
-public class App {
 	public static  void main(String[] args) {
-		System.out.println("Hello World!");;
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+
 	}
 }
---
+++ src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,75 @@
+package hexlet.code;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
+
+
+class Differ {
+    
+	public static String generate(String file1, String file2) throws Exception {
+
+		Map <String, Object> map1 = getData(file1);
+		Map <String, Object> map2 = getData(file2);
+		Map <String, Object> difMap = new TreeMap <>();
+
+		for (Map.Entry<String ,Object> entry : map1.entrySet()) {
+			String key1 = entry.getKey();
+			if (map2.containsKey(key1)) {
+				if (map2.get(key1).equals(entry.getValue())) {
+					difMap.put(key1 + "  ", entry.getValue());
+				} else {
+					difMap.put(key1 + "1-", entry.getValue());
+					difMap.put(key1 + "2+", map2.get(key1));
+				}
+			} else {
+				difMap.put(key1 + " -", entry.getValue());
+			}
+		}
+
+		for (Map.Entry<String ,Object> entry : map2.entrySet()) {
+			if (!map1.containsKey(entry.getKey())) {
+				difMap.put(entry.getKey() + " +" , entry.getValue());
+			}
+		}
+
+		StringBuilder difJson = new StringBuilder();
+
+		difJson.append("{\n");
+		for (Map.Entry<String ,Object> entry : difMap.entrySet()) {
+			String key = entry.getKey();
+			difJson.append(" ");
+			difJson.append(key.substring(key.length() - 1));
+			difJson.append(" ");
+			difJson.append(key.substring(0, key.length() - 2));
+			difJson.append(": ");
+			difJson.append(entry.getValue().toString());
+			difJson.append("\n");
+		}
+		difJson.append("}\n");
+
+		return difJson.toString();
+	}
+
+	public static Map parse(String file) throws Exception  {
+
+		Path path = Paths.get(file);
+        String content = null;
+
+        content = Files.readString(path);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.readValue(content, new TypeReference<Map<String, Object>>(){});
+    }
+
+    public static Map getData(String content) throws Exception {
+        return parse(content);
+    }
+
+} 
